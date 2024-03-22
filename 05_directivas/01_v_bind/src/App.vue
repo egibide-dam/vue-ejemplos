d69332bc38e0c18d3ed0fe0d10efec050d8abeed
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-03-22 12:13:09 +0100

    Proyecto plantilla con Bootstrap

---
+++ 05_directivas/01_v_bind/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <h1>Ejemplo Vue.js</h1>
+    <button class="btn btn-primary me-2">Primario</button>
+    <button class="btn btn-secondary">Secundario</button>
+</template>
+
+<style scoped>
+</style>
