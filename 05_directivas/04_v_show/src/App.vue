f0effd31fccd165e2c3aba04071b7eb50b920fca
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-03-22 17:04:31 +0100

    Ejemplo show

---
+++ 05_directivas/04_v_show/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+const edad_if = ref(16);
+const edad_show = ref(16);
+</script>
+
+<template>
+    <h1>v-if</h1>
+    <div class="card col-4 p-3 mb-5">
+        <label class="form-label" for="edad_if">Edad</label>
+        <input class="form-control" id="edad_if" type="number" min="0" max="120" v-model="edad_if"/>
+        <p class="text-danger" v-if="edad_if >= 18">Es mayor de edad.</p>
+    </div>
+    <h1>v-show</h1>
+    <div class="card col-4 p-3">
+        <label class="form-label" for="edad_show">Edad</label>
+        <input class="form-control" id="edad_show" type="number" min="0" max="120" v-model="edad_show"/>
+        <p class="text-danger" v-show="edad_show >= 18">Es mayor de edad.</p>
+    </div>
+</template>
+
+<style scoped>
+</style>
