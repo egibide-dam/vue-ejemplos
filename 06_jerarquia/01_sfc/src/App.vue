dd2cfa5ad8b2f40a1f0af3c37a5fd92b72c14a2d
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-03-25 19:45:42 +0100

    Ejemplo SFC

---
+++ 06_jerarquia/01_sfc/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NuevoComponente from "@/components/NuevoComponente.vue";
+</script>
+
+<template>
+    <h1>Ejemplo de Single File Component</h1>
+    <NuevoComponente/>
+</template>
+
+<style scoped>
+</style>
---
+++ 06_jerarquia/01_sfc/src/components/NuevoComponente.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <h2>Hola, soy el nuevo.</h2>
+</template>
+
+<style scoped>
+</style>
