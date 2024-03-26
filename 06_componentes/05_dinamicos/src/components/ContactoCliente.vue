de4c91cdefdbb693173c386cfe55dc7d64577186
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-03-26 15:06:22 +0100

    Ejemplo de componentes dinámicos

---
+++ 06_componentes/05_dinamicos/src/components/ContactoCliente.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+    <h2>Completa el formulario</h2>
+    <div class="card col-4 p-3">
+        <div class="mb-3">
+            <label class="form-label" for="mensaje">Mensaje</label>
+            <input class="form-control" placeholder="Escribe algo..." id="mensaje" type="text"/>
+        </div>
+        <div>
+            <button class="btn btn-primary">Enviar</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
