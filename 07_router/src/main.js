0f41815bc8ed9ab183b17dd28a70df558d313db5
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-03-26 23:17:21 +0100

    Integrar Bootstrap

--- 07_router/src/main.js
+++ 07_router/src/main.js
@@ -1,4 +1,8 @@
-import './assets/main.css'
+// Import our custom CSS
+import './assets/bootstrap.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 import { createApp } from 'vue'
 import App from './App.vue'
