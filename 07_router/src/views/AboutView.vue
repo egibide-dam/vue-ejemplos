d94648f6d07c789324146ada20c7adf27274a201
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-03-27 00:29:57 +0100

    Añadida una ruta con nombre

--- 07_router/src/views/AboutView.vue
+++ 07_router/src/views/AboutView.vue
@@ -1,9 +1,13 @@
 <script setup>
 import Lipsum from "@/components/Lipsum.vue";
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
     <h2>Acerca de</h2>
     <hr>
     <Lipsum/>
+    <RouterLink class="link-primary link-underline-opacity-0 link-underline-opacity-100-hover" :to="{ name: 'home' }">
+        Volver a la página principal
+    </RouterLink>
 </template>
