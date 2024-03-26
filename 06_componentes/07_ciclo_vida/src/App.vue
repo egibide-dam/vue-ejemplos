e0946184e0e553b5cf22e036808769d57d26564e
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-03-26 17:15:47 +0100

    Ejemplo de ciclo de vida

--- 06_componentes/07_ciclo_vida/src/App.vue
+++ 06_componentes/07_ciclo_vida/src/App.vue
@@ -1,8 +1,28 @@
 <script setup>
+import { ref } from 'vue';
+import ContactoCliente from '@/components/ContactoCliente.vue';
+import ProductosCatalogo from '@/components/ProductosCatalogo.vue';
+
+const opciones = {
+    ProductosCatalogo,
+    ContactoCliente
+}
+
+const seleccion = ref('ContactoCliente');
 </script>
 
 <template>
     <h1>Ejemplo de ciclo de vida</h1>
+    <nav class="navbar navbar-expand bg-body-secondary mb-5">
+        <div class="navbar-nav px-3">
+            <button class="nav-link link-primary" @click="seleccion='ProductosCatalogo'">Catálogo de productos</button>
+            <div class="vr mx-3"/>
+            <button class="nav-link link-primary" @click="seleccion='ContactoCliente'">Formulario de contacto</button>
+        </div>
+    </nav>
+    <main>
+        <component :is="opciones[seleccion]"></component>
+    </main>
 </template>
 
 <style scoped>
