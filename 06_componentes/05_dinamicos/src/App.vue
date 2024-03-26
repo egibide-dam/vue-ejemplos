ec5e4f7c16b6dd5d97f95d6b90714828491a646f
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-03-26 12:29:22 +0100

    Proyectos

---
+++ 06_componentes/05_dinamicos/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <h1>Ejemplo de componentes dinámicos</h1>
+</template>
+
+<style scoped>
+</style>
---
+++ 06_componentes/06_asincronos/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <h1>Ejemplo de componentes asíncronos</h1>
+</template>
+
+<style scoped>
+</style>
