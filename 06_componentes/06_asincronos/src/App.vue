8da1a2587579c83545dee44291da68e0b7498201
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-03-26 15:22:21 +0100

    Ejemplo de componente asíncrono

--- 06_componentes/06_asincronos/src/App.vue
+++ 06_componentes/06_asincronos/src/App.vue
@@ -1,8 +1,23 @@
 <script setup>
+import { defineAsyncComponent, ref } from 'vue';
+
+const HijoAsync = defineAsyncComponent(() => import ('@/components/HijoAsync.vue'));
+
+const activo = ref(false);
+
+// https://vuejs.org/guide/components/async.html
 </script>
 
 <template>
     <h1>Ejemplo de componentes asíncronos</h1>
+    <nav class="navbar navbar-expand bg-body-secondary mb-5">
+        <div class="navbar-nav px-3">
+            <button class="nav-link link-primary" @click="activo = !activo">Cargar componente hijo</button>
+        </div>
+    </nav>
+    <main>
+        <HijoAsync v-if="activo"/>
+    </main>
 </template>
 
 <style scoped>
