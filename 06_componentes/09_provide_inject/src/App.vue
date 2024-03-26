bd9930a407b7a276540cb080c519fec93da44ffe
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-03-26 22:53:19 +0100

    Ejemplo de provide/inject

--- 06_componentes/09_provide_inject/src/App.vue
+++ 06_componentes/09_provide_inject/src/App.vue
@@ -1,8 +1,24 @@
 <script setup>
+import { provide, ref } from 'vue';
+import HijoComponente from './components/HijoComponente.vue'
+
+const frase = ref('Hola, ¿qué tal?');
+
+// provide(clave, valor)
+provide('fraseANieto', frase);
 </script>
 
 <template>
     <h1>Ejemplo de provide/inject</h1>
+    <div class="card col-4 p-3">
+        <div class="mb-3">
+            <label class="form-label" for="frase">Frase</label>
+            <input class="form-control" id="frase" type="text" placeholder="Escribe algo..." v-model="frase"/>
+        </div>
+    </div>
+    <div class="col-4 mt-5">
+        <HijoComponente/>
+    </div>
 </template>
 
 <style scoped>
